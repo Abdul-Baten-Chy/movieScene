277eb184b3f1fbdd5d5375e9a3ce4f918d43b87c
Abdull Baten <abdulbatinchy@gmail.com>
2026-01-03 23:11:24 +0600

    add remove item from cart

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,16 +2,19 @@ import sun from "@/assets/icons/sun.svg";
 import logo from "@/assets/logo.svg";
 import ring from "@/assets/ring.svg";
 import shoppingCart from "@/assets/shopping-cart.svg";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import Cart from "../feature/cart/Cart";
 import { useCartContext } from "../hooks/useCartContext";
 
 function Header() {
-  const { state } = useCartContext();
+  const { state, dispatch } = useCartContext();
+  const [showCart, setShowCart] = useState(false);
   const cartData = state.cart;
   const cardCount = useMemo(() => cartData.length, [cartData]);
 
   return (
     <header>
+      {showCart && <Cart onClose={setShowCart} cart={state.cart} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
@@ -35,7 +38,10 @@ function Header() {
             </a>
           </li>
           <li>
-            <button className="bg-primary/20 dark:bg-primary/[7%] relative rounded-lg backdrop-blur-[2px] p-1 inline-block">
+            <button
+              onClick={() => setShowCart(true)}
+              className="bg-primary/20 dark:bg-primary/[7%] relative rounded-lg backdrop-blur-[2px] p-1 inline-block"
+            >
               <img src={shoppingCart} width="24" height="24" alt="" />
               {cardCount > 0 && (
                 <span className="absolute -top-2 -right-2 bg-primary text-dark text-xs w-5 h-5 flex items-center justify-center rounded-full ring-2 ring-body">
